99fb29e6296a175d7ab43a267d5b3fd6af503572
Paul Lopez <p.lopez@alienor.net>
2024-02-09 12:14:18 +0100

    test approval test_elections_without_district

--- tests/ElectionsTest.php
+++ tests/ElectionsTest.php
@@ -2,20 +2,51 @@
 
 namespace Tests\Elections\ElectionKata;
 
+use ApprovalTests\Approvals;
+use Elections\ElectionKata\Elections;
 use PHPUnit\Framework\TestCase;
 use function PHPUnit\Framework\assertTrue;
 
 class ElectionsTest extends TestCase
 {
+    const JOHNNY = 'Johnny';
+    const BOB = 'Bob';
+    const JERRY = 'Jerry';
+    const ANNA = 'Anna';
+    const MICHEL = 'Michel';
+    const JESS = "Jess";
+    const JULY = "July";
+    const SIMON = "Simon";
+    const MATT = "Matt";
+    const CAROLE = "Carole";
+    const DISTRICT_1 = 'District 1';
+    const DISTRICT_2 = 'District 2';
+    const DISTRICT_3 = "District 3";
+    const DONALD = "Donald";
+    const JOE = "Joe";
     private $list = [
-        "District 1" => ["Bob", "Anna", "Jess", "July"],
-        "District 2" => ["Jerry", "Simon"],
-        "District 3" => ["Johnny", "Matt", "Carole"],
+        self::DISTRICT_1 => [self::BOB, self::ANNA, self::JESS, self::JULY],
+        self::DISTRICT_2 => [self::JERRY, self::SIMON],
+        self::DISTRICT_3 => [self::JOHNNY, self::MATT, self::CAROLE],
     ];
 
     public function test_elections_without_district(): void
     {
-        assertTrue(true);
+        $election = new Elections($this->list, false);
+        $election->addCandidate(self::MICHEL);
+        $election->addCandidate(self::JERRY);
+        $election->addCandidate(self::JOHNNY);
+        
+        $election->voteFor(self::BOB, self::JERRY, self::DISTRICT_1);
+        $election->voteFor(self::JERRY, self::JERRY, self::DISTRICT_2);
+        $election->voteFor(self::ANNA, self::JOHNNY, self::DISTRICT_1);
+        $election->voteFor(self::JOHNNY, self::JOHNNY, self::DISTRICT_3);
+        $election->voteFor(self::MATT, self::DONALD, self::DISTRICT_3);
+        $election->voteFor(self::JESS, self::JOE, self::DISTRICT_1);
+        $election->voteFor(self::SIMON, "", self::DISTRICT_2);
+        $election->voteFor(self::CAROLE, "", self::DISTRICT_3);
+
+        Approvals::verifyList($election->results());
     }
 
     public function test_elections_with_district(): void
